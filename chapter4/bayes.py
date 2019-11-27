5a47744f1027f8b66073a23be02668a1e0bffdb3
yangguang <71194506061@stu.ecnu.edu.cn>
2019-11-27 17:47:32 +0800

    chapter04 done

--- chapter4/bayes.py
+++ chapter4/bayes.py
@@ -1,4 +1,5 @@
-from numpy import *
+#-*- coding:utf-8 -*-
+import numpy as np
 
 def loadDataSet():
     postingList=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
@@ -15,7 +16,7 @@ def createVocabList(dataSet):
     for document in dataSet:
         vocabSet = vocabSet | set(document)
     return list(vocabSet)
-
+# 次集模型 每个词只出现一次 只关心每个词出现与否
 def setOfWords2Vec(vocabList, inputSet):
     returnVec = [0] * len(vocabList)
     for word in inputSet:
@@ -28,11 +29,11 @@ def setOfWords2Vec(vocabList, inputSet):
 def trainNB0(trainMatrix, trainCategory):
     numTrainDocs = len(trainMatrix)
     numWords = len(trainMatrix[0])
-    pAbusive = sum(trainCategory)/float(numTrainDocs)
-    p0Num = zeros(numWords)
-    p1Num = zeros(numWords)
-    p0Denom = 0.0
-    p1Denom = 0.0
+    pAbusive = np.sum(trainCategory)/float(numTrainDocs)
+    p0Num = np.ones(numWords)
+    p1Num = np.ones(numWords)
+    p0Denom = 2.0
+    p1Denom = 2.0
     for i in range(numTrainDocs):
         if trainCategory[i] == 1:
             p1Num += trainMatrix[i]
@@ -40,6 +41,81 @@ def trainNB0(trainMatrix, trainCategory):
         else:
             p0Num += trainMatrix[i]
             p0Denom += sum(trainMatrix[i])
-    p1Vect = p1Num/p1Denom
-    p0Vect = p0Num/p0Denom
+    p1Vect = np.log(p1Num/p1Denom)
+    p0Vect = np.log(p0Num/p0Denom)
     return p0Vect, p1Vect, pAbusive
+
+def classifyNB(vec2Classify, p0Vec, p1Vec, pClass1):
+    p1 = np.sum(vec2Classify * p1Vec) + np.log(pClass1)
+    p0 = np.sum(vec2Classify * p0Vec) + np.log(1.0 - pClass1)
+    if p1 > p0:
+        return 1
+    else:
+        return 0
+
+def testingNB():
+    listOPosts, listClasses = loadDataSet()
+    myVocabList = createVocabList(listOPosts)
+    trainMat = []
+    for postindoc in listOPosts:
+        trainMat.append(setOfWords2Vec(myVocabList, postindoc))
+    p0V, p1V, pAb = trainNB0(np.array(trainMat), np.array(listClasses))
+    testEntry = ['love', 'my', 'dalmation']
+    thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))
+    print(testEntry, 'classified as: ', classifyNB(thisDoc,
+    p0V, p1V, pAb))
+    testEntry = ['stupid', 'garbage']
+    thisDoc = np.array(setOfWords2Vec(myVocabList, testEntry))
+    print(testEntry, 'classified as: ', classifyNB(thisDoc,
+    p0V, p1V, pAb))
+
+# 词袋模型 会考虑词出现的次数
+def bagOfWords2VecMN(vocabList, inputSet):
+    returnVec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] += 1
+    return returnVec
+
+## 文本解析
+def textParse(bigString):
+    import re
+    # 这里正则表达还不清楚 作用是用bigString中非字母数字的字符作为分隔符
+    # 不得行 看来正则还需要好好研究一番
+    #listOfTokens = re.split(r'\w*', bigString)
+    listOfTokens = bigString.split()
+    return [tok.lower() for tok in listOfTokens if len(tok) > 2]
+
+def spamTest():
+    docList, classList, fullText = [], [], []
+    for i in range(1,26):
+        wordList = textParse(open('email/spam/%d.txt' % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)
+        wordList = textParse(open('email/ham/%d.txt' % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+    vocabList = createVocabList(docList)
+    trainingSet = list(range(50))
+    testSet = []
+    for i in range(10):
+        randIndex = int(np.random.uniform(0,len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del(trainingSet[randIndex])
+    trainMat, trainClasses = [], []
+    for docIndex in trainingSet:
+        trainMat.append(setOfWords2Vec(vocabList, docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+    p0V, p1V, pSpam = trainNB0(np.array(trainMat), np.array(trainClasses))
+    errorCount = 0
+    for docIndex in testSet:
+        wordVector = setOfWords2Vec(vocabList, docList[docIndex])
+        if classifyNB(np.array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:
+            errorCount += 1
+            print(docList[docIndex], classList[docIndex])
+    print('the error rate is : %f' % (float(errorCount)/len(testSet)))
+    return float(errorCount)/len(testSet)
+
+# 最后一个例子由于数据不知道怎么获取 故先不做了
